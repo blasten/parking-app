c954ed0d7981727a500d2ebc97e64b6516876be9
CountBronislaw <tcarney1337@gmail.com>
2014-04-16 22:40:37 -0400

    Added REST contract to contain constants.

---
+++ android/NKUParkingLocator/src/team1/parkingapp/rest/RestContract.java
@@ -0,0 +1,39 @@
+/*
+ * RestContract.java
+ * 4/16/14
+ * Travis Carney
+ * 
+ * This class only contains constants that are used by the other REST classes.
+ */
+package team1.parkingapp.rest;
+
+public final class RestContract {
+	// API URL constants here
+	protected static final String BASE_API_URL 		= 	"http://parking-app.herokuapp.com/api/";
+	protected static final String USERS_API 		= 	BASE_API_URL + "users/";
+	protected static final String SPOTS_API			= 	BASE_API_URL + "spots/";
+	protected static final String LOTS_API			=	BASE_API_URL + "lots/";
+	protected static final String RESERVATIONS_API	=	BASE_API_URL + "reservations/";
+	
+	// User API key-value parameters
+	// POST parameters
+	protected static final String EMAIL 			= 	"email";
+	protected static final String PASSWORD			= 	"password";
+	protected static final String NAME		 		=	"name";
+	protected static final String LASTNAME			= 	"lastname";
+	
+	// Spots API key-value parameters
+	// GET parameters
+	protected static final String LAT1				= 	"lat1";
+	protected static final String LONG1				= 	"long1";
+	protected static final String LAT2				= 	"lat2";
+	protected static final String LONG2				= 	"long2";
+	
+	// Lots API key-value parameters
+	// There are none that can be placed into constants
+	
+	// Reservation API key-value parameters
+	// POST & PUT parameters
+	protected static final String SPOT_ID			=	"spot_id";
+	protected static final String STATUS			=	"status";
+}
